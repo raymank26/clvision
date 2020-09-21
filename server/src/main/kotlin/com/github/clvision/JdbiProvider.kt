36a2a71a6254deac4f5060de747c7179dbfd3aff
Anton Ermak <rayman.k26@gmail.com>
2020-09-21 16:44:21 +0300

    Not used code.

--- server/src/main/kotlin/com/github/clvision/JdbiProvider.kt
+++ server/src/main/kotlin/com/github/clvision/JdbiProvider.kt
@@ -5,7 +5,6 @@ import org.jdbi.v3.core.Jdbi
 class JdbiProvider {
     fun provide(url: String): Jdbi {
         val jdbi = Jdbi.create(url)
-        jdbi.registerArrayType(String::class.java, "string")
         return jdbi
     }
 }
