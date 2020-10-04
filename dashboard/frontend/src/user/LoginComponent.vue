129b5c08bb1aae83fb608640493afb1678c40239
Anton Ermak <rayman.k26@gmail.com>
2020-10-04 21:30:04 +0300

    More tests.

--- dashboard/frontend/src/user/LoginComponent.vue
+++ dashboard/frontend/src/user/LoginComponent.vue
@@ -1,9 +1,20 @@
 <template>
-    <form v-on:submit.prevent="submit">
-        <input id="username" name="username" v-model="username">
-        <input id="password" name="password" v-model="password">
-        <button id="submit" type="submit">Submit</button>
-    </form>
+    <div>
+        <form v-on:submit.prevent="submit">
+            <input id="username" name="username" v-model="username">
+            <input id="password" name="password" v-model="password">
+            <button id="submit" type="submit">Submit</button>
+        </form>
+        <div v-if="errorType === 'no input'">
+            Username or password is not set.
+        </div>
+        <div v-else-if="errorType === 'mismatch'">
+            Illegal username or password.
+        </div>
+        <div v-else-if="errorType === 'unknown'">
+            Unknown error
+        </div>
+    </div>
 </template>
 
 <script lang="ts">
@@ -14,18 +25,24 @@ export default class LoginComponent extends Vue {
 
     username: string = "";
     password: string = "";
-
-    created() {
-        console.log("HERE2");
-    }
+    errorType: string = "";
 
     submit() {
-        console.log("SUBMIT");
+        if (!this.username || !this.password) {
+            this.errorType = "no input";
+            return;
+        }
         this.$store.dispatch("login", {
             "username": this.username,
             "password": this.password
         }).then(() => {
             this.$router.push("/dashboard")
+        }).catch(error => {
+            if (error.key === "illegal.credits") {
+                this.errorType = "mismatch";
+            } else {
+                this.errorType = "unknown";
+            }
         });
     }
 }
--- dashboard/frontend/tests/unit/LogComponent.spec.ts
+++ dashboard/frontend/tests/unit/LogComponent.spec.ts
@@ -5,13 +5,13 @@ import {createStore} from "@/store";
 import {UserService} from "@/user/UserService";
 import {ApiService} from "@/ApiService";
 import Vue from "vue";
-import {VueRouter} from "vue-router/types/router";
 
 const localVue = createLocalVue();
-
 localVue.use(Vuex);
 
-describe('RegistrationComponent.vue', () => {
+const USER_NAME = "anton.ermak";
+
+describe('LoginComponent.vue', () => {
     let store: Store<any>;
     let apiService: ApiService;
     let wrapper: Wrapper<LoginComponent>;
@@ -19,10 +19,16 @@ describe('RegistrationComponent.vue', () => {
 
     beforeEach(() => {
         apiService = {
-            get: jest.fn(), post: jest.fn(() => {
-                return Promise.resolve<any>({
-                    username: "anton.ermak"
-                });
+            get: jest.fn(), post: jest.fn((url, config) => {
+                if ((config.bodyParams as any)["username"] === USER_NAME) {
+                    return Promise.resolve<any>({
+                        username: USER_NAME
+                    });
+                } else {
+                    return Promise.reject<any>({
+                        key: "illegal.credits"
+                    });
+                }
             })
         };
         store = createStore(new UserService(apiService));
@@ -47,7 +53,7 @@ describe('RegistrationComponent.vue', () => {
     it('Login form submits content', async done => {
         let username = wrapper.find("#username");
         let password = wrapper.find("#password");
-        username.setValue("anton.ermak");
+        username.setValue(USER_NAME);
         password.setValue("123892");
         let submitButton = wrapper.find("#submit");
         submitButton.trigger("click");
@@ -57,6 +63,39 @@ describe('RegistrationComponent.vue', () => {
 
         await Vue.nextTick();
         expect($router.push.mock.calls[0][0]).toBe("/dashboard");
+        done()
+    });
+
+    it('Login form shows error on empty input', async done => {
+        let expectedError = "Username or password is not set";
+
+        expect(wrapper.html()).not.toContain(expectedError);
+
+        let submitButton = wrapper.find("#submit");
+        submitButton.trigger("click");
+
+        await Vue.nextTick();
+
+        expect(wrapper.html()).toContain(expectedError);
+
+        done();
+    });
+
+    it("Login form shows error on illegal input", async done => {
+        let expectedError = "Illegal username or password";
+        expect(wrapper.html()).not.toContain(expectedError);
+
+        let username = wrapper.find("#username");
+        let password = wrapper.find("#password");
+        username.setValue("foo");
+        password.setValue("bar");
+        let submitButton = wrapper.find("#submit");
+        submitButton.trigger("click");
+
+        await Vue.nextTick();
+        await Vue.nextTick();
+        await Vue.nextTick();
+        expect(wrapper.html()).toContain(expectedError);
         done()
     });
 
